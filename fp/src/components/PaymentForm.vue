5464ec81f84aa884b9861194ca99946064582b6a
Niiaz <niiaz.gabidullin@gmail.com>
2022-03-10 12:19:59 +0300

    home work 3

---
+++ fp/src/components/PaymentForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <input placeholder="Date" v-model="date">
+      <input placeholder="Categoru" v-model="categoru">
+      <input placeholder="Price" v-model.number="price">
+      <button @click="save">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        date:"",
+        categoru: "",
+        price:0
+    }
+},
+methods: {
+    save(){
+        const {date, categoru, price} = this
+        this.$emit("add",{date,categoru,price})
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
